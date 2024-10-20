10379afcc21d7b53e3a068384cedc414be7341e7
praveenanand333 <praveenanand333@gmail.com>
2024-10-20 10:59:31 +0530

    com

--- Crypto-Security-Lab/07-08/vignere.cpp
+++ Crypto-Security-Lab/07-08/vignere.cpp
@@ -2,43 +2,43 @@
 #include <string>
 using namespace std;
 
-// Function to remove spaces from the key
+
 string prepareKey(string key, int msgLength) {
     string newKey = key;
     while (newKey.length() < msgLength) {
-        newKey += key; // Repeat the key until it's the same length as the message
+        newKey += key; 
     }
-    return newKey.substr(0, msgLength); // Trim the key to match the message length
+    return newKey.substr(0, msgLength); 
 }
 
-// Function to encrypt the message
+
 string encryptVigenere(string message, string key) {
     string cipherText = "";
     string preparedKey = prepareKey(key, message.length());
     
     for (int i = 0; i < message.length(); i++) {
         if (message[i] != ' ') {
-            char encryptedChar = (message[i] + preparedKey[i] - 2 * 'A') % 26 + 'A'; // Encrypt
+            char encryptedChar = (message[i] + preparedKey[i] - 2 * 'A') % 26 + 'A'; 
             cipherText += encryptedChar;
         } else {
-            cipherText += message[i]; // Preserve spaces
+            cipherText += message[i]; 
         }
     }
     
     return cipherText;
 }
 
-// Function to decrypt the message
+
 string decryptVigenere(string cipherText, string key) {
     string message = "";
     string preparedKey = prepareKey(key, cipherText.length());
     
     for (int i = 0; i < cipherText.length(); i++) {
         if (cipherText[i] != ' ') {
-            char decryptedChar = (cipherText[i] - preparedKey[i] + 26) % 26 + 'A'; // Decrypt
+            char decryptedChar = (cipherText[i] - preparedKey[i] + 26) % 26 + 'A'; 
             message += decryptedChar;
         } else {
-            message += cipherText[i]; // Preserve spaces
+            message += cipherText[i]; 
         }
     }
     

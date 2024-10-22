5b23cbb156a798fdfaff070045a4ffc49de8848c
praveenanand333 <praveenanand333@gmail.com>
2024-10-22 22:01:02 +0530

    abs-update

--- Crypto-Security-Lab/rc4.cpp
+++ Crypto-Security-Lab/rc4.cpp
@@ -1,69 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
 using namespace std;
 
-class RC4 {
-private:
-    vector<int> S;
-    int i, j;
 
-public:
-    RC4(const string& key) {
-        S.resize(256);
-        for (int k = 0; k < 256; k++) {
-            S[k] = k;
-        }
+void KSA(vector<int> &S, const string &key) {
+    int keyLength = key.size();
+    int j = 0;
 
-        j = 0;
-        for (int k = 0; k < 256; k++) {
-            j = (j + S[k] + key[k % key.length()]) % 256;
-            swap(S[k], S[j]);
-        }
-
-        i = 0;
-        j = 0;
+    
+    for (int i = 0; i < 256; i++) {
+        S[i] = i;
     }
 
-    string encrypt(const string& plaintext) {
-        string ciphertext;
-        for (char c : plaintext) {
-            i = (i + 1) % 256;
-            j = (j + S[i]) % 256;
-            swap(S[i], S[j]);
-            int K = S[(S[i] + S[j]) % 256];
-            ciphertext += c ^ K;
-        }
-        return ciphertext;
+    
+    for (int i = 0; i < 256; i++) {
+        j = (j + S[i] + key[i % keyLength]) % 256;
+        swap(S[i], S[j]);
+    }
+}
+
+
+string PRGA(vector<int> &S, const string &data) {
+    int i = 0, j = 0;
+    string output = data;
+
+    for (int k = 0; k < data.size(); k++) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        swap(S[i], S[j]);
+
+        
+        int keyStream = S[(S[i] + S[j]) % 256];
+        output[k] = data[k] ^ keyStream;
     }
 
-    string decrypt(const string& ciphertext) {
-        return encrypt(ciphertext);
-    }
-};
+    return output;
+}
+
+
+string RC4(const string &key, const string &data) {
+    vector<int> S(256);
+    KSA(S, key);           
+    return PRGA(S, data);   
+}
 
 int main() {
-    string key;
-    cout << "Enter the key: ";
-    cin >> key;
+    string key = "secret_key";
+    string plaintext = "Hello, World!";
 
-    string plaintext;
-    cout << "Enter the plaintext: ";
-    cin.ignore(); 
-    getline(cin, plaintext);
-
-    RC4 rc4(key);
-    string ciphertext = rc4.encrypt(plaintext);
-
-    cout << "Ciphertext (in hex): ";
-    for (char c : ciphertext) {
-        printf("%02x ", (unsigned char)c);
+    
+    string ciphertext = RC4(key, plaintext);
+    cout << "Ciphertext: ";
+    for (unsigned char c : ciphertext) {
+        printf("%02X ", c);  
     }
     cout << endl;
 
-    string decryptedText = rc4.decrypt(ciphertext);
-    cout << "Decrypted text: " << decryptedText << endl;
+    
+    string decrypted = RC4(key, ciphertext);
+    cout << "Decrypted text: " << decrypted << endl;
 
     return 0;
 }
